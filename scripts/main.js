4a41c4a83ca46485f522eedb646d6175abd1c3d2
João Pedro Machado <imjoaomachado@gmail.com>
2022-08-09 13:16:01 -0300

    add files

---
+++ scripts/main.js
@@ -0,0 +1,117 @@
+const codificarBtn = document.getElementById("criptografar");
+const decodificarBtn = document.getElementById("descriptografar");
+const copyBtn = document.getElementById("copyBtn");
+const container = document.querySelector(".container");
+const mostrarModal = document.querySelector(".modal-content");
+const mostrarTexto = document.querySelector(".user-output");
+const click = new Audio("./assets/audio/click.wav");
+var texto = document.getElementById("user_Text");
+var texto2 = document.getElementById("user_Text2");
+var entrada = texto.value;
+var saida = texto2.value;
+
+mostrarModal.style.display = "flex";
+mostrarTexto.style.display = "none";
+
+codificarBtn.addEventListener("click", codificar);
+decodificarBtn.addEventListener("click", decodificar);
+copyBtn.addEventListener("click", copy);
+
+function codificar() {
+    mostrarTela();
+    let entrada = texto.value;
+
+    texto2.value = "";
+
+    for(let i=0; i < entrada.length; i++){
+        if ((entrada[i] != entrada[i].toUpperCase()) || entrada[i] == " ") {
+            if (entrada[i] == "a") {
+                texto2.value += "ai";
+            } else if (entrada[i] == "e") {
+                texto2.value += "enter";
+            } else if (entrada[i] == "i") {
+                texto2.value += "imes";
+            } else if (entrada[i] == "o") {
+                texto2.value += "ober";
+            } else if (entrada[i] == "u") {
+                texto2.value += "ufat";
+            } else {
+                texto2.value += entrada[i];
+            }
+        }else{
+            alert("Não é permitido letras maiusculas!!")
+        }
+    }
+}
+
+function decodificar() {
+    mostrarTela();
+    let entrada = texto.value;
+
+    texto2.value = "";
+
+    for(let i=0; i < entrada.length; i++){
+        if ((entrada[i] != entrada[i].toUpperCase()) || entrada[i] == " ") {
+            if (entrada[i] == "ai") {
+                texto2.value += "a";
+            } else if (entrada[i] == "enter") {
+                texto2.value += "e";
+            } else if (entrada[i] == "imes") {
+                texto2.value += "i";
+            } else if (entrada[i] == "ober") {
+                texto2.value += "o";
+            } else if (entrada[i] == "ufat") {
+                texto2.value += "u";
+            } else {
+                texto2.value += entrada[i];
+            }
+        }else{
+            alert("Não é permitido letras maiusculas!!")
+        }
+    }
+}
+
+
+function copy(){
+    let copyText = document.querySelector("#user_Text2");
+    copyText.select();
+    let textoCopiado = document.execCommand("copy");
+
+    if (textoCopiado == true) {
+        createNotification();
+        click.play();
+        clear();
+    }else {
+        textoCopiado = false;
+    }
+    return;
+};
+
+function createNotification() {
+    const ntf = document.createElement("div");
+    ntf.classList.add("modal-ntf");
+
+    ntf.innerText = "Copiado com sucesso!!!"
+
+    container.appendChild(ntf)
+
+    setTimeout(() => {
+        ntf.remove() 
+    }, 3000);
+
+}
+
+function mostrarTela() {
+    mostrarTexto.style.display = "flex";
+    mostrarModal.style.display = "none";
+    
+    if (texto.value == false) {
+        mostrarTexto.style.display = "none";
+        mostrarModal.style.display = "flex";
+    }
+    return;
+}
+
+function clear() {
+    return texto.value = "";
+}
